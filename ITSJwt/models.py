a1faa4d8ecd916ba62e8ed3b99699a7e5a796965
oh <dhwogus1212@naver.com>
2022-05-23 10:15:01 +0900

    Revert "Social login+ JWT complete"
    This reverts commit 5732050c3d156e7b6442015824efff949075e7c7.

--- ITSJwt/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    name=models.CharField(max_length=50,null=True)
-    picture=models.URLField(max_length=2000,null=True)
-    class Meta:
-        swappable = 'AUTH_USER_MODEL'
